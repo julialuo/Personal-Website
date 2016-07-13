107f590896630100542bf148188ab479aeafb1af
Julia Luo <juliazhiyuan.luo@gmail.com>
2016-07-13 12:06:20 -0400

    Added JS with JQuery and Waypoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,37 @@
 /**
  * Created by julia on 2016-07-11.
  */
-var previous_id = "home-nav"
 
-function nav_click (button_id) {
-    document.getElementById(previous_id).className = "";
-    document.getElementById(button_id).className = "active";
-    previous_id = button_id;
-}
+$(document).ready(function() {
+    var previous_id = "home-nav";
+
+    $('#home-nav, #about-nav, #projects-nav, #contact-nav').click(function () {
+        $('#' + previous_id).removeClass('active');
+        $(this).addClass("active");
+        previous_id = this.id;
+    });
+
+    $('#home').waypoint(function() {
+        $('#' + previous_id).removeClass('active');
+        $('#home-nav').addClass("active");
+        previous_id = 'home-nav';
+    });
+
+    $('#about').waypoint(function() {
+        $('#' + previous_id).removeClass('active');
+        $('#about-nav').addClass("active");
+        previous_id = 'about-nav';
+    });
+
+    $('#projects').waypoint(function() {
+        $('#' + previous_id).removeClass('active');
+        $('#projects-nav').addClass("active");
+        previous_id = 'projects-nav';
+    });
+
+    $('#contact').waypoint(function() {
+        $('#' + previous_id).removeClass('active');
+        $('#contact-nav').addClass("active");
+        previous_id = 'contact-nav';
+    });
+});
