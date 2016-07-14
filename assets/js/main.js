004c152eec7b85372604f94588fd82746d73a41c
Julia Luo <juliazhiyuan.luo@gmail.com>
2016-07-13 23:25:15 -0400

    Tweaked Waypoints and some CSS changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,35 +3,77 @@
  */
 
 $(document).ready(function() {
-    var previous_id = "home-nav";
+    var previous_id = 'home-nav';
 
-    $('#home-nav, #about-nav, #projects-nav, #contact-nav').click(function () {
-        $('#' + previous_id).removeClass('active');
-        $(this).addClass("active");
-        previous_id = this.id;
+    $('#home').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('#' + previous_id).css('background-color', 'rgb(9, 38, 68)');
+            $('#home-nav').css('background-color', 'rgb(181, 170, 255)');
+            previous_id = 'home-nav';
+        }
     });
 
-    $('#home').waypoint(function() {
-        $('#' + previous_id).removeClass('active');
-        $('#home-nav').addClass("active");
-        previous_id = 'home-nav';
+    $('#home').waypoint(function(direction) {
+        if (direction == 'up') {
+            $('#' + previous_id).css('background-color', 'rgb(9, 38, 68)');
+            $('#home-nav').css('background-color', 'rgb(181, 170, 255)');
+            previous_id = 'home-nav';
+        }
+    }, {
+        offset: '-1%'
     });
 
-    $('#about').waypoint(function() {
-        $('#' + previous_id).removeClass('active');
-        $('#about-nav').addClass("active");
-        previous_id = 'about-nav';
+    $('#about').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('#' + previous_id).css('background-color', 'rgb(9, 38, 68)');
+            $('#about-nav').css('background-color', 'rgb(138, 255, 138)');
+            previous_id = 'about-nav';
+        }
     });
 
-    $('#projects').waypoint(function() {
-        $('#' + previous_id).removeClass('active');
-        $('#projects-nav').addClass("active");
-        previous_id = 'projects-nav';
+    $('#about').waypoint(function(direction) {
+        if (direction == 'up') {
+            $('#' + previous_id).css('background-color', 'rgb(9, 38, 68)');
+            $('#about-nav').css('background-color', 'rgb(138, 255, 138)');
+            previous_id = 'about-nav';
+        }
+    }, {
+        offset: '-1%'
     });
 
-    $('#contact').waypoint(function() {
-        $('#' + previous_id).removeClass('active');
-        $('#contact-nav').addClass("active");
-        previous_id = 'contact-nav';
+    $('#projects').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('#' + previous_id).css('background-color', 'rgb(9, 38, 68)');
+            $('#projects-nav').css('background-color', 'rgb(255, 179, 181)');
+            previous_id = 'projects-nav';
+        }
+    });
+
+    $('#projects').waypoint(function(direction) {
+        if (direction == 'up') {
+            $('#' + previous_id).css('background-color', 'rgb(9, 38, 68)');
+            $('#projects-nav').css('background-color', 'rgb(255, 179, 181)');
+            previous_id = 'projects-nav';
+        }
+    }, {
+        offset: '-1%'
+    });
+
+    $('#contact').waypoint(function(direction) {
+        if (direction == 'down') {
+            $('#' + previous_id).css('background-color', 'rgb(9, 38, 68)');
+            $('#contact-nav').css('background-color', 'rgb(255, 188, 121)');
+            previous_id = 'contact-nav';
+        }
+    });
+
+    $('#contact').waypoint(function(direction) {
+        if (direction == 'up') {
+            $('#' + previous_id).css('background-color', 'rgb(9, 38, 68)');
+            $('#contact-nav').css('background-color', 'rgb(255, 188, 121)');
+            previous_id = 'contact-nav';
+        }
+    }, {
+        offset: '-1%'
     });
 });
